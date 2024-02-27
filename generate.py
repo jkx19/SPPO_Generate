079c3eb2b683a41214e8dff865e089194946a7cd
Jerry <kaixuanji@cs.ucla.edu>
2024-02-26 17:00:49 -0800

    fixed max length argument

--- generate.py
+++ generate.py
@@ -19,7 +19,6 @@ warnings.filterwarnings("ignore")
 kwargs = InitProcessGroupKwargs(timeout=timedelta(seconds=36000))
 accelerator = Accelerator(kwargs_handlers=[kwargs])
 
-from alignment.model_utils import is_adapter_model
 from peft import PeftConfig, PeftModel
 
 def parse_arguments():
@@ -63,56 +62,21 @@ def main():
     output_dir.mkdir(parents=True, exist_ok=True)
 
     dataset = "HuggingFaceH4/ultrafeedback_binarized"
+    data = load_dataset(dataset, split="train_prefs")
+    prompts = ["### Instruction: " + data[idx]["prompt"] + "\n\n### Response: " for idx in range(len(data))]
 
-    # data = load_dataset(dataset, split="train_prefs")
-        # print(data[0]["chosen"])
-    # exit()
-    # prompts = ["### Instruction: " + data[idx]["prompt"] + "\n\n### Response: " for idx in range(len(data))]
-    f = open("fractions/full_prompts.json")
-    prompts = json.load(f)
-    f.close()
-
-    # f = open(f"fractions/{data_frac}_of_8.json")
-    # idx_prompts_all = json.load(f)
-    # f.close()
-    # prompts = [x[1] for x in idx_prompts_all]
-
-    model = model_path
-    # if is_adapter_model(model, None) is True:
-    #     peft_config = PeftConfig.from_pretrained(model_path, revision=None)
-    #     model_kwargs = dict(
-    #         revision=None,
-    #         torch_dtype=torch.bfloat16,
-    #         device_map={"": accelerator.process_index},
-    #     )
-    #     base_model = AutoModelForCausalLM.from_pretrained(
-    #         peft_config.base_model_name_or_path,
-    #         **model_kwargs,
-    #     )
-    #     model = PeftModel.from_pretrained(
-    #         base_model,
-    #         model_path,
-    #         revision=None
-    #     )
-    #     tokenizer = AutoTokenizer.from_pretrained(model_path)   
-    #     tokenizer.pad_token = tokenizer.eos_token
-    
-    # else:
-    # model = AutoModelForCausalLM.from_pretrained(
-    #     model_path,    
-    #     device_map={"": accelerator.process_index},
-    #     torch_dtype=torch.bfloat16,
-    # )
     tokenizer = AutoTokenizer.from_pretrained(model_path)   
     tokenizer.pad_token = tokenizer.eos_token
     llm = LLM(
         model=model_path,
         tensor_parallel_size=args.world_size,
     )
-    sampling_params = SamplingParams(temperature=1.0, top_p=1.0, max_tokens=512)
+    sampling_params = SamplingParams(temperature=1.0, top_p=1.0, max_tokens=args.maxlen)
 
 
     pairs = args.pairs   
+
+    os.makedirs(args.output_dir, exist_ok=True)
 
     for p in range(pairs):
         response = llm.generate(prompts, sampling_params)
@@ -121,54 +85,7 @@ def main():
         json.dump(output, f)
         f.close()
 
-        # print("-------------------------------------------------------")
-        # print(output[0])
-
-    # results = [[] for i in range(pairs)]
-    
-    # batch_size = args.batch_size
-    # prompt_batches=[prompts[i:i + batch_size] for i in range(0, len(prompts), batch_size)] 
-    # tokenizer.padding_side="left"
-
-    # for prompts_batch in tqdm(prompt_batches):
-    #     prompts_tokenized = tokenizer(
-    #             prompts_batch, 
-    #             return_tensors="pt", 
-    #             padding='longest', 
-    #             truncation=True,
-    #             max_length=1024, 
-    #             pad_to_multiple_of=8,
-    #             add_special_tokens=False).to("cuda") 
-    #     # set max_new_tokens smaller for faster inference
-
-    #     for p in range(pairs):
-    #         # outputs_tokenized=model.generate(**prompts_tokenized, max_new_tokens=256, pad_token_id=tokenizer.eos_token_id, do_sample=True)
-    #         outputs_tokenized = llm.generate()
-
-    #         # remove prompt from gen. tokens
-    #         outputs_tokenized=[ tok_out[len(tok_in):] 
-    #             for tok_in, tok_out in zip(prompts_tokenized["input_ids"], outputs_tokenized) ] 
-    #         # decode gen. tokens 
-    #         outputs=tokenizer.batch_decode(outputs_tokenized)
-    #         results[p].extend(outputs)
-        # exit()
-        # results.extend(outputs)
-
-
-    # if accelerator.is_local_main_process:
-    #     timediff=time.time()-start
-    #     print(f"time elapsed: {timediff}")
-
-    #     # collecting data
-    #     for idx in range(len(chosen_all)):
-    #         d = {"chosen": [{"role": "user", "content": prompts_old[idx]}, {"role": "assistant", "content": chosen_all[idx]}], "generated": [{"role": "user", "content": prompts_old[idx]}, {"role": "assistant", "content": results[idx]}]}
-    #         if args.split == 'test':
-    #             filename = f"{args.output_dir}/loser_{data_frac}_test.jsonl"
-    #         else:
-    #             filename = f"{args.output_dir}/loser_{data_frac}.jsonl"
-    #         with open(filename, 'a') as f:
-    #             json.dump(d, f)
-    #             f.write('\n')
+        
 
 
 if __name__ == "__main__":
